6860464cd84e3f8c7239f3bfec5d9f94e37cec3f
uhjpgighp40 <79582917+uhjpgighp40@users.noreply.github.com>
2021-03-04 21:51:20 +0800

    Add files via upload

---
+++ 第3堂作業03.py
@@ -0,0 +1,7 @@
+for i in range(1,10):
+    
+
+    for j in range(1,11-i):
+               print(j,end='')
+    print()
+ #end=' '意思是末尾不换行，加空格。
---
+++ 第3堂作業04.py
@@ -0,0 +1,7 @@
+sum=0
+e = int(input("請輸入數值:"))
+for i in range(1,e+1):
+  
+ print(i)
+ sum+=i  
+print('數值加總=',sum)     
